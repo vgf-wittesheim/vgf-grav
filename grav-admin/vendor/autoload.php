ca9b2b5263dee1f38012ba437d8ae8a32b3ea63c
Tobias Schneck <tobias.schneck@consol.de>
2017-10-03 19:13:56 +0200

    update grav version

--- grav-admin/vendor/autoload.php
+++ grav-admin/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitcc0bfb0747d68462acd177d00a576744::getLoader();
+return ComposerAutoloaderInitebad8ca6e0ad645d471dfb21f54c56c5::getLoader();
